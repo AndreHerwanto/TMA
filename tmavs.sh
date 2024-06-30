6c8d3404eb0686e44680ccd06f4c2b88677368ab
WANZ <herwantoandre@gmail.com>
2024-06-30 16:15:52 +0700

    push
    commit

---
+++ tmavs.sh
@@ -0,0 +1,4 @@
+sudo wget -P /home/linux/TMA/ "http://192.168.212.41/DokumentasiVPS/files/appl/TMServerAgentLinux441812980269.tar"
+tar -vfx /home/linux/TMA/TMServerAgentLinux441812980269.tar
+cd /home/linux
+sudo .tmbc install
