6cce00e10d516ce5593391a83e49c62ef67089ff
WANZ <herwantoandre@gmail.com>
2024-06-30 16:33:23 +0700

    push
    final

--- tmavs.sh
+++ tmavs.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+
+# Download file menggunakan wget
 sudo wget -P /home/linux/TMA/ "http://192.168.212.41/DokumentasiVPS/files/appl/TMServerAgentLinux441812980269.tar"
+
+# Ekstrak file tar
 tar -vfx /home/linux/TMA/TMServerAgentLinux441812980269.tar
+
+# Pindah ke direktori /home/linux
 cd /home/linux
+
+# Jalankan instalasi
 sudo .tmbc install
